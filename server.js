c4e7618e3e9068b7e6c7c50f6b220f95dc0a511d
Shriyash05 <shrimhatre00@gmail.com>
2026-06-15 18:19:04 +0530

    database fix

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || (isProduction ? '' : 'aurey
 const DB_PATH = path.join(__dirname, 'database.json');
 const LOCAL_UPLOAD_DIR = path.join(__dirname, 'public', 'uploads');
 const DATABASE_URL = process.env.DATABASE_URL || process.env.POSTGRES_URL;
-
+console.log("DATABASE_URL =", process.env.DATABASE_URL);
+console.log("POSTGRES_URL =", process.env.POSTGRES_URL);
 const { Pool } = pg;
 const pgPool = DATABASE_URL
   ? new Pool({
